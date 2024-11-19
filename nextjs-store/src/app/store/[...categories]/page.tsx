b60958a47b717750c588f2cc192fe557e30ea6cd
JorgeMS <bugsmario9510@gmail.com>
2024-11-18 23:28:16 -0500

    rutas dinamicas

---
+++ nextjs-store/src/app/store/[...categories]/page.tsx
@@ -0,0 +1,16 @@
+
+//Aqui se crea la interfaz que es una buena practica para saber que datos va a traer
+interface CategoryProps {
+    params:{
+        categories:string[]
+    }
+}
+
+
+export default function categories(props:CategoryProps){
+    const {categories}=props.params
+    console.log(categories)
+    return (
+    <h1> Esta es la pagina de categoria:{categories}</h1> 
+)
+}
