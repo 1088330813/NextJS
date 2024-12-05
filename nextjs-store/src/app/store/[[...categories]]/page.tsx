a5b9413d0935fad747eef169b338ce4ac3c434cb
JorgeMS <bugsmario9510@gmail.com>
2024-12-05 12:24:04 -0500

    primer commit con React

--- nextjs-store/src/app/store/[[...categories]]/page.tsx
+++ nextjs-store/src/app/store/[[...categories]]/page.tsx
@@ -2,14 +2,14 @@
 //Aqui se crea la interfaz que es una buena practica para saber que datos va a traer
 interface CategoryProps {
     params:{
-        categories:string[]
+        categories:string[],
+        searchParams?: string[]
     }
 }
 
-
 export default function categories(props:CategoryProps){
     const {categories}=props.params
-    console.log(categories)
+    console.log(categories)     
     return (
     <h1> Esta es la pagina de categoria:{categories}</h1> 
 )
--- nextjs-store/src/app/store/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Store(){
-    return (
-        <h1>Este es mi store</h1>
-    )
-}
