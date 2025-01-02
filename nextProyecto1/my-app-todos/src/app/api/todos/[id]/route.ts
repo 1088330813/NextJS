8b54d14075553517e1a3395f1f0d5464d8c696c8
1088330813 <78113562+1088330813@users.noreply.github.com>
2025-01-02 12:21:27 -0500

    solucion de bugs 2

--- nextProyecto1/my-app-todos/src/app/api/todos/[id]/route.ts
+++ nextProyecto1/my-app-todos/src/app/api/todos/[id]/route.ts
@@ -3,12 +3,15 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+  //Asi se obtiene el id desde la URL
+const obtenerIdDesdeUrl= (url:string):number|null=>{ //este indica que entra un string a la funcion y que va a retornar un valor o un nulo
+  const id = parseInt(new URL (url).pathname.split("/").pop() as string);
+  return Number.isNaN(id)?null:id;
+};
+
 export async function DELETE(req: Request) {
   try {
-    //Asi se obtiene el id desde la URL
-    const url = new URL(req.url);
-    const id = parseInt(url.pathname.split("/").pop() as string);
-
+  const id=obtenerIdDesdeUrl(req.url);
     if (!id) {
       return NextResponse.json(
         { error: "El ID es requerido" },
@@ -33,8 +36,7 @@ export async function DELETE(req: Request) {
 export async function PATCH(req: Request) {
   try {
     //Asi se obtiene el id desde la URL
-    const url = new URL(req.url);
-    const id = parseInt(url.pathname.split("/").pop() as string);
+    const id = obtenerIdDesdeUrl(req.url);
 
     if (!id) {
       return NextResponse.json(
@@ -59,10 +61,14 @@ export async function PATCH(req: Request) {
           {status:404}
         );
       }
-      const objectiveTime= new Date(registroActual.objectiveTime);
-
-      const diferenceTime=nuevaFechaModificada ? (objectiveTime.getTime()-nuevaFechaModificada.getTime())/60000:null;//divido por 60000 para poder sacar los minutos
+      const objectiveTime= registroActual.objectiveTime ? new Date(registroActual.objectiveTime):null;
+      let diferenceTime:number | null = null;
+      if(objectiveTime&&nuevaFechaModificada){
+      diferenceTime=
+     (objectiveTime.getTime()-nuevaFechaModificada.getTime())/60000;//divido por 60000 para poder sacar los minutos
       //se podria usar math.abs, cuando necesitemos traer solo numeros positivos
+      }
+
 
     const updatedTodo= await prisma.todos.update({
       where: { id },
